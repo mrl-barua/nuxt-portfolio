eeb3b67fc366f7f706d5fe5af065f8e880b56ba5
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2024-06-21 23:19:19 +0800

    chore: Remove unnecessary code from app.vue

---
+++ pages/hero.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2>Hero</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro a
+      quam ipsam alias consequuntur quisquam rem ut ullam eaque et perferendis
+      pariatur ipsa ea deserunt soluta laboriosam inventore tempora!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro a
+      quam ipsam alias consequuntur quisquam rem ut ullam eaque et perferendis
+      pariatur ipsa ea deserunt soluta laboriosam inventore tempora!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
